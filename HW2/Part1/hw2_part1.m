71a3df0e5b3d78faa7609a801dc41501f85728c6
hydai <z54981220@gmail.com>
2015-04-20 21:03:20 +0800

    Finish hw2-part1-b

--- HW2/Part1/hw2_part1.m
+++ HW2/Part1/hw2_part1.m
@@ -3,11 +3,11 @@ darkFigure();
 catImage = im2double(imread('github_icon.png'));
 [h, w, ~] = size(catImage);
 imshow(catImage);
-
+load('ctrlptlist_64.mat');
 %% Mouse input
 xlabel ('Select at most 200 points along the outline', 'FontName', '微軟正黑體', 'FontSize', 14);
-[ ctrlPointX, ctrlPointY ] = ginput(200);
-ctrlPointList = [ctrlPointX ctrlPointY];
+%[ ctrlPointX, ctrlPointY ] = ginput(200);
+%ctrlPointList = [ctrlPointX ctrlPointY];
 clickedN = size(ctrlPointList,1);
 
 promptStr = sprintf('%d points selected', clickedN);
@@ -18,7 +18,7 @@ xlabel (promptStr, 'FontName', '微軟正黑體', 'FontSize', 14);
 
 %% Calculate Bzier curve (Your efforts here)
 %outlineVertexList = ctrlPointList; %Enrich outlineVertexList
-sample_size = 40;
+sample_size = 100;
 t = linspace(0, 1, sample_size); % [0, 1] 取樣 sample_size
 M = [-1 3 -3 1; 3 -6 3 0; -3 3 0 0; 1 0 0 0];
 
@@ -36,6 +36,13 @@ for verI = 1:3:clickedN
 end
 %% Draw and fill the polygon
 drawAndFillPolygon( catImage, ctrlPointList, outlineVertexList, true, true, true ); %ctrlPointScattered, polygonPlotted, filled
-
+figure;
+fprintf('PA done\n');
 %% ===================================================
 % Part 1 (b)
+catImageX4 = imresize(catImage, 4, 'nearest');
+ctrlPointList4 = ctrlPointList * 4;
+outlineVertexList4 = outlineVertexList * 4;
+drawAndFillPolygon( catImageX4, ctrlPointList4, outlineVertexList4, true, true, true ); %ctrlPointScattered, polygonPlotted, filled
+
+fprintf('PB done\n');

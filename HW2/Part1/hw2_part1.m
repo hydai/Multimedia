18339eb86c4a731d547ed2eb9f9c1066552e5e18
hydai <z54981220@gmail.com>
2015-04-20 20:55:43 +0800

    Finish hw2-part1-a

--- HW2/Part1/hw2_part1.m
+++ HW2/Part1/hw2_part1.m
@@ -13,8 +13,29 @@ clickedN = size(ctrlPointList,1);
 promptStr = sprintf('%d points selected', clickedN);
 xlabel (promptStr, 'FontName', '微軟正黑體', 'FontSize', 14);
 
-%% Calculate Bㄚˇzier curve (Your efforts here)
-outlineVertexList = ctrlPointList; %Enrich outlineVertexList
+%% ===================================================
+% Part 1 (a)
 
+%% Calculate Bzier curve (Your efforts here)
+%outlineVertexList = ctrlPointList; %Enrich outlineVertexList
+sample_size = 40;
+t = linspace(0, 1, sample_size); % [0, 1] 取樣 sample_size
+M = [-1 3 -3 1; 3 -6 3 0; -3 3 0 0; 1 0 0 0];
+
+outlineVertexList = [];
+for verI = 1:3:clickedN
+    G = zeros(4, 2);
+    for verJ = 0:3
+        xx = mod(verI, clickedN)+1;
+        G(verJ+1, :) = ctrlPointList(xx, :);
+    end
+    for samK = 1:sample_size
+        T = [t(samK).^3, t(samK).^2, t(samK), 1];
+        outlineVertexList = [outlineVertexList; T*M*G];
+    end
+end
 %% Draw and fill the polygon
 drawAndFillPolygon( catImage, ctrlPointList, outlineVertexList, true, true, true ); %ctrlPointScattered, polygonPlotted, filled
+
+%% ===================================================
+% Part 1 (b)
